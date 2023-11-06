af18e1835aeaa5803e1b05e4c403d51c426f2a1c
Kiran Hipparagi <49370990+KiranHipparagi@users.noreply.github.com>
2023-11-06 14:43:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkPassword() {
     var password = document.getElementById("password").value;
     if (password === "5111998") {
-        document.getElementById("message").innerText = "Congratulations! You got a special gift for your birthday! 🎉";
+        window.location.href = "gift.html";
     } else {
         document.getElementById("message").innerText = "Incorrect password. Try again.";
     }
